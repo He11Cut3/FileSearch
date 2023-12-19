35af4bd2e35f639d335e701fe41a83eaf882c779
Hells <96310786+He11Cut3@users.noreply.github.com>
2023-12-19 21:24:32 +0300

    v1

---
+++ FileSearchCatalog.Tests/ProgramTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+public class ProgramTests : IDisposable
+{
+    private readonly string testDirectory;
+    private readonly string testDataDirectory;
+    private readonly string searchStringFilePath;
+
+    public ProgramTests()
+    {
+        // Создаем временную папку на рабочем столе
+        testDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "TestDirectory");
+        Directory.CreateDirectory(testDirectory);
+
+        // Создаем подкаталог для тестовых данных
+        testDataDirectory = Path.Combine(testDirectory, "TestData");
+        Directory.CreateDirectory(testDataDirectory);
+
+        // Создаем файл SearchString.txt и записываем в него "SearchString"
+        searchStringFilePath = Path.Combine(testDataDirectory, "SearchString.txt");
+        File.WriteAllText(searchStringFilePath, "SearchString");
+    }
+
+    [Fact]
+    public void SearchTextInFiles_ShouldFindMatchingLines() //Проверка корректности поиска строки в файлах.
+    {
+        // Arrange
+        var directoryPath = testDataDirectory;
+        var searchString = "SearchString";
+        var resultLines = new List<string>();
+
+        // Создаем файл с тестовыми данными
+        var testFilePath = Path.Combine(directoryPath, "TestFile.txt");
+        File.WriteAllText(testFilePath, "'Это строка с SearchString.");
+
+        // Act
+        Program.SearchTextInFiles(directoryPath, searchString, resultLines);
+
+        // Assert
+        Assert.NotEmpty(resultLines);
+        Assert.All(resultLines, line => Assert.Contains(searchString, line));
+    }
+
+
+    [Fact]
+    public void SearchTextInFiles_WhenSearchStringNotFound_ShouldNotAddToResultLines() //Проверка поведения при отсутствии искомой строки.
+    {
+        // Arrange
+        var directoryPath = Path.Combine(testDirectory, "TestData");
+        var searchString = "NonexistentString";
+        var resultLines = new List<string>();
+
+        // Act
+        Program.SearchTextInFiles(directoryPath, searchString, resultLines);
+
+        // Assert
+        Assert.Empty(resultLines);
+    }
+
+    [Fact]
+    public void SearchTextInFiles_WhenSearchStringFound_ShouldAddToResultLines() //Проверка корректности добавления строк при наличии искомой строки.
+    {
+        // Arrange
+        var directoryPath = Path.Combine(testDirectory, "TestData");
+        var searchString = "SearchString";
+        var resultLines = new List<string>();
+
+        // Act
+        Program.SearchTextInFiles(directoryPath, searchString, resultLines);
+
+        // Assert
+        Assert.NotEmpty(resultLines);
+        Assert.All(resultLines, line => Assert.Contains(searchString, line));
+    }
+
+    [Fact]
+    public void GetRelativePath_ShouldReturnCorrectRelativePath() //Проверка корректности получения относительного пути.
+    {
+        // Arrange
+        var basePath = testDataDirectory;
+        var fullPath = searchStringFilePath;
+
+        // Act
+        var result = Program.GetRelativePath(basePath, fullPath);
+
+        // Assert
+        var expected = Path.Combine("TestData", "SearchString.txt");
+        Assert.Equal(expected, result);
+    }
+    public void Dispose() //Для удаления временной папки после завершения всех тестов.
+    {
+        Directory.Delete(testDirectory, true);
+    }
+}
---
+++ FileSearchCatalog/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите путь: ");
+        string directoryPath = Console.ReadLine(); // Ввод пути.
+
+        Console.Write("Напишите, что необходимо найти: ");
+        string searchString = Console.ReadLine(); // Ввод фразы, необходимую найти.
+
+        if (!Directory.Exists(directoryPath)) // Проверка на существование пути.
+        {
+            Console.WriteLine($"Путь '{directoryPath}' не найден.");
+            return;
+        }
+
+        List<string> resultLines = new List<string>(); // Создания списка
+
+        SearchTextInFiles(directoryPath, searchString, resultLines); // "SearchTextInFiles" - функция, которая реализует поиск.
+                                                                     // ( где все параметры: directoryPath - путь,  searchString - "слово", resultLines - список из всех найденный "совпадений".
+
+        if (resultLines.Count > 0) // Если есть совпадения
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); // Скачивает файл на рабочий стол.
+            string resultFilePath = Path.Combine(desktopPath, "Результат.txt"); //Сочетает путь и наименование файла.
+
+            File.WriteAllLines(resultFilePath, resultLines); //Записывание файла.
+            Console.WriteLine($"Результат сохранён '{resultFilePath}'."); //Вывод информации.
+        }
+        else
+        {
+            Console.WriteLine("Таких строк не найдено."); //Если нету такой фразы в файлах.
+        }
+    }
+
+    public static void SearchTextInFiles(string directoryPath, string searchString, List<string> resultLines)
+    {
+        foreach (string filePath in Directory.EnumerateFiles(directoryPath, "*.txt", SearchOption.AllDirectories)) // Поиск пути ко всем файлам в каталоге и его подкаталогах.
+        {
+            int lineNumber = 0;
+            foreach (string line in File.ReadLines(filePath)) // После поиска файла в каталогах - чтение файла.
+            {
+                lineNumber++; // Подсчёт строк.
+                if (line.Contains(searchString)) // Содержит ли в себе строку, которую изначально искали?
+                {
+                    string result = $"{GetRelativePath(directoryPath, filePath)}({lineNumber}): {line}"; 
+                    resultLines.Add(result); // Если да, то записывает в результат данную строку.
+                }
+            }
+        }
+    }
+
+
+    public static string GetRelativePath(string basePath, string fullPath) // Возращает относительный путь от basePath к fullPath.
+    {
+        Uri baseUri = new Uri(basePath);
+        Uri fullUri = new Uri(fullPath);
+        return Uri.UnescapeDataString(baseUri.MakeRelativeUri(fullUri).ToString().Replace('/', Path.DirectorySeparatorChar));
+    }
+}
